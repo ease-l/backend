785f212ed67817bca73142d04940f21527ce665f
MikhniukR <r.mikhniuk@gmail.com>
2016-05-28 20:01:14 +0300

    Исправил view после тестирования

--- DB/MongoClientFactory.cs
+++ DB/MongoClientFactory.cs
@@ -5,23 +5,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace DB
 {
     public class MongoClientFactory
     {
         public static MongoClient GetMongoClient()
-        {
+        {                        
             var dbConnectionString = System.Configuration.ConfigurationManager.AppSettings["DB"];
             if (!String.IsNullOrEmpty(dbConnectionString))
             {
                 return new MongoClient(dbConnectionString);
             }
             return new MongoClient();
+            
         }
-
+        
         public static IMongoDatabase GetMongoDatabase(String name = "mongodb")
-        {
+        {            
             return GetMongoClient().GetDatabase(name);
         }
     }

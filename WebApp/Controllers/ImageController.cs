0aab0e9a0139266ff4a4b92367bd330e5efb5bf6
MikhniukR <r.mikhniuk@gmail.com>
2016-06-16 16:59:21 +0300

    Поменял картинку

--- WebApp/Controllers/ImageController.cs
+++ WebApp/Controllers/ImageController.cs
@@ -22,7 +22,7 @@ namespace WebApp.Controllers
             image.Name = "Test";
             image.CreationelData = new DateTime(2016, 9, 1, 0, 0, 0);
             image.Version = 1;
-            image.Url = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQm4syVfMekd8XqRYu0jOgVpMgYLiWZAjw1Te5gQqE0pwmdT9zY7nb-VOer";
+            image.Url = "http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D1%8D-%D0%9A%D0%BB%D0%B8%D0%BA%D0%B0%D0%B1%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE-%D0%BE%D0%B1%D0%BE%D0%B8-%D0%BA%D1%80%D0%B0%D1%81%D0%B8%D0%B2%D1%8B%D0%B5-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-2629498.jpeg";
             images.Add(image);
             //Betta data 
             return Json(images, JsonRequestBehavior.AllowGet);
@@ -43,7 +43,7 @@ namespace WebApp.Controllers
             image.Name = "Test";
             image.CreationelData = new DateTime(2016, 9, 1, 0, 0, 0);
             image.Version = 1;
-            image.Url = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQm4syVfMekd8XqRYu0jOgVpMgYLiWZAjw1Te5gQqE0pwmdT9zY7nb-VOer";
+            image.Url = "http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D1%8D-%D0%9A%D0%BB%D0%B8%D0%BA%D0%B0%D0%B1%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE-%D0%BE%D0%B1%D0%BE%D0%B8-%D0%BA%D1%80%D0%B0%D1%81%D0%B8%D0%B2%D1%8B%D0%B5-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-2629498.jpeg";
             images1.Add(image);
             return Json(images1, JsonRequestBehavior.AllowGet);
             //Betta data 

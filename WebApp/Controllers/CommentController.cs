8321609a2fd04e7dfbcc740f94483b274d8dd98e
MikhniukR <r.mikhniuk@gmail.com>
2016-06-22 14:53:39 +0300

    Оно стало работать, продолжаю проверку

--- WebApp/Controllers/CommentController.cs
+++ WebApp/Controllers/CommentController.cs
@@ -16,8 +16,8 @@ namespace WebApp.Controllers
         public JsonResult Index()
         {
             //Beta data
-            var o = Newtonsoft.Json.JsonConvert.DeserializeObject<List<CommentWithoutObjectId>>(@"[{""Id"":""5764fd5efcfbb421280ee61e"",""Author"":""000000000000000000000000"",""Version"":1,""Name"":""Simple comment"",""CreationelData"":""\/Date(1467320400000)\/"",""Text"":""The best comment to image in project in project""},{""Id"":""5764ff18fcfbb423487e7f1a"",""Author"":""000000000000000000000000"",""Version"":3,""Name"":""Simple comment 2"",""CreationelData"":""\/Date(1467320400000)\/"",""Text"":""The best comment to root project""}]");
-            return Json(o, JsonRequestBehavior.AllowGet);
+            /*var o = Newtonsoft.Json.JsonConvert.DeserializeObject<List<CommentWithoutObjectId>>(@"[{""Id"":""5764fd5efcfbb421280ee61e"",""Author"":""000000000000000000000000"",""Version"":1,""Name"":""Simple comment"",""CreationelData"":""\/Date(1467320400000)\/"",""Text"":""The best comment to image in project in project""},{""Id"":""5764ff18fcfbb423487e7f1a"",""Author"":""000000000000000000000000"",""Version"":3,""Name"":""Simple comment 2"",""CreationelData"":""\/Date(1467320400000)\/"",""Text"":""The best comment to root project""}]");
+            return Json(o, JsonRequestBehavior.AllowGet);*/
             //Beta data
             var comments = CommentWithoutObjectId.CommentsToCommentWithoutObjectId(_commentRepository.GetAllComment());
             return Json(comments, JsonRequestBehavior.AllowGet);

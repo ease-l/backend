b3551f15cc2b61cefff8d5cb956ed9d74851e361
MikhniukR <r.mikhniuk@gmail.com>
2016-08-06 21:04:46 +0300

    Сделал ImageLogic

---
+++ ControllersLogic/Logic/ImageLogic.cs
@@ -0,0 +1,150 @@
+﻿using ControllersLogic.Interfaces;
+using DB.Interfaces;
+using DB.Models;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+
+namespace ControllersLogic.Logic
+{
+    public class ImageLogic : IImageLogic
+    {
+        private IImageRepository _imageRepository = new DB.Repositories.DBImageRepository();
+        private ICommentRepository _commentRepository = new DB.Repositories.DBCommentRepository();
+
+        public String AddCommentToImage(String simageId, String text, String name)
+        {
+            Comment comment = new Comment();
+            comment.Text = text;
+            comment.CreationelData = DateTime.UtcNow;
+            comment.Name = name;
+            comment.Version = 1;
+            var commentId = _commentRepository.AddComment(comment).Id;
+            var imageId = new ObjectId();
+            if (!ObjectId.TryParse(simageId, out imageId))
+            {
+                return "Bad id it's not objectId";
+            }
+            if (_imageRepository.GetImageById(imageId) == null)
+            {
+                return "Bad id image";
+            }
+            _imageRepository.AddCommentToImage(commentId, imageId);
+            return commentId.ToString();
+        }
+        public String AddImage(String url, String name)
+        {
+            Image image = new Image();
+            image.Url = url;
+            image.Version = 1;
+            image.Name = name;
+            image.CreationelData = DateTime.UtcNow;
+            var id = _imageRepository.AddImage(image).Id.ToString();
+            return id;
+        }
+        public String DeleteCommentFromImage(String idImage, String idComment)
+        {
+            var objectIdImage = new ObjectId();
+            var objectIdComment = new ObjectId();
+            if (!ObjectId.TryParse(idImage, out objectIdImage))
+            {
+                return "Bad image id";
+            }
+            if (!ObjectId.TryParse(idComment, out objectIdComment))
+            {
+                return "Bad comment id";
+            }
+            if (_imageRepository.GetImageById(objectIdImage) == null)
+            {
+                return "Bad image id, not found in DB";
+            }
+            _imageRepository.DeleteCommentFromImage(objectIdImage, objectIdComment);
+            _commentRepository.DeleteById(objectIdComment);
+            return "OK";
+        }
+        public String DownloadImage(HttpPostedFileBase uploadImage)
+        {
+            if (uploadImage != null)
+            {
+                string fileName = Path.GetFileName(uploadImage.FileName);
+                var input = uploadImage.InputStream;
+                CloudinaryDotNet.Account account = new CloudinaryDotNet.Account("hzvwvtbls", "482455376217895", "bXPz-CiQrEjZp4xqSV8UK_nfI2c");
+                CloudinaryDotNet.Cloudinary cloudinary = new CloudinaryDotNet.Cloudinary(account);
+                CloudinaryDotNet.Actions.ImageUploadParams uploadParams = new CloudinaryDotNet.Actions.ImageUploadParams()
+                {
+                    File = new CloudinaryDotNet.Actions.FileDescription(fileName, input)
+                };
+                CloudinaryDotNet.Actions.ImageUploadResult uploadResult = cloudinary.Upload(uploadParams);
+                string url = cloudinary.Api.UrlImgUp.BuildUrl(String.Format("{0}.{1}", uploadResult.PublicId, uploadResult.Format));
+                return url;
+            }
+            else
+            {
+                return "Bad file";
+            }
+        }
+        public List<ImageWithoutObjectId> GetAllImage()
+        {
+            var images = ImageWithoutObjectId.ImagesToImageWithoutObjectId(_imageRepository.GetAllImage());
+            return images;
+        }
+        public ImageWithoutObjectId GetById(String id)
+        {
+            var objectId = new ObjectId();
+            if (!ObjectId.TryParse(id, out objectId))
+            {
+                throw new Exception("Bad id it's not objectId");
+            }
+            if (objectId == null || _imageRepository.GetImageById(objectId) == null)
+            {
+                throw new Exception("Bad id");
+            }
+            var image = ImageWithoutObjectId.ImageToImageWithoutObjectId(_imageRepository.GetImageById(objectId));
+            if (image == null)
+            {
+                throw new Exception("Bad id");
+            }
+            return image;
+        }
+        public List<CommentWithoutObjectId> GetCommentFromImage(String simageId)
+        {
+            var imageId = new ObjectId();
+            if (!ObjectId.TryParse(simageId, out imageId))
+            {
+                throw new Exception("Bad id it's not objectId");
+            }
+            var image = _imageRepository.GetImageById(imageId);
+            if (image == null)
+            {
+                throw new Exception("Bad id image, this image don't found in DB");
+            }
+            var comments = CommentWithoutObjectId.CommentsToCommentWithoutObjectId(_commentRepository.GetCommentsByIds(image.Comments));
+            return comments;
+        }
+        public ImageWithoutObjectId UpdateById(String id, String name, String url)
+        {
+            var objectId = new ObjectId();
+            if (!ObjectId.TryParse(id, out objectId))
+            {
+                throw new Exception("Bad id it's not objectId");
+            }
+            if (objectId == null)
+            {
+                throw new Exception("Bad id");
+            }
+            var image = _imageRepository.GetImageById(objectId);
+            if (image == null)
+            {
+                throw new Exception("Bad id");
+            }
+            image.Url = url;
+            image.Version++;
+            image.Name = name;
+            _imageRepository.DeleteById(objectId);
+            _imageRepository.AddImage(image);
+            return ImageWithoutObjectId.ImageToImageWithoutObjectId(image);
+        }
+    }
+}
---
+++ WebApp/Controllers/ImageApiController.cs
@@ -0,0 +1,57 @@
+﻿using ControllersLogic.Interfaces;
+using ControllersLogic.Logic;
+using DB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    public class ImageApiController
+    {
+        private IImageLogic _imageLogic = new ImageLogic();
+
+        [HttpPost, Route("api/Image/id{simageId}/comment")]
+        public String  AddCommentToImage(String simageId, String text, String name)
+        {
+            return _imageLogic.AddCommentToImage(simageId, text, name);
+        }
+        [HttpPost, Route("api/Image")]
+        public String AddImage(String url, String name)
+        {
+            return _imageLogic.AddImage(url, name);
+        }
+        [HttpDelete, Route("api/Image/id{idImage}/comment/id{idComment}")]
+        public String DeleteCommentFromImage(String idImage, String idComment)
+        {
+            return _imageLogic.DeleteCommentFromImage(idImage, idComment);
+        }
+        [HttpPost, Route("api/Image/Download")]
+        public String DownloadImage(HttpPostedFileBase uploadImage)
+        {
+            return _imageLogic.DownloadImage(uploadImage);
+        }
+        [HttpGet, Route("api/Image/id{id}")]
+        public ImageWithoutObjectId GetById(String id)
+        {
+            return _imageLogic.GetById(id);
+        }
+        [HttpGet, Route("api/Image/id{simageId}/comment")]
+        public List<CommentWithoutObjectId> GetCommentFromImage(String simageId)
+        {
+            return _imageLogic.GetCommentFromImage(simageId);
+        }
+        [HttpGet, Route("api/Image")]
+        public List<ImageWithoutObjectId> index()
+        {
+            return _imageLogic.GetAllImage();
+        }
+        [HttpPut, Route("api/Image/id{id}")]
+        public ImageWithoutObjectId UpdateById(String id, String name, String url)
+        {
+            return _imageLogic.UpdateById(id, name, url);
+        }
+    }
+}
